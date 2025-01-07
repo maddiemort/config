58e304f0fd24e8e6d78bfe112c3bcdffbe5c1d39
Madeleine Mortensen <me@maddie.wtf>
2025-01-07 14:14:44 +0000

    chore(hx): finish configuring helix

--- home/users/maddie.nix
+++ home/users/maddie.nix
@@ -239,16 +239,105 @@
       package = pkgsUnstable.helix;
       settings = {
         theme = "catppuccin_macchiato";
-        editor.cursor-shape = {
-          normal = "block";
-          insert = "bar";
-          select = "block";
-        };
-        editor.end-of-line-diagnostics = "hint";
-        editor.inline-diagnostics = {
-          cursor-line = "error";
+
+        editor = {
+          scrolloff = 3;
+          scroll-lines = 2;
+          shell = [ "fish" "-c" ];
+          line-number = "relative";
+          cursorline = true;
+          auto-completion = false;
+          preview-completion-insert = false;
+          auto-info = true;
+          color-modes = true;
+          text-width = 100;
+
+          statusline = {
+            left = [
+              "mode"
+              "spacer"
+              "spinner"
+              "spacer"
+              "diagnostics"
+              "file-name"
+              "read-only-indicator"
+              "file-modification-indicator"
+            ];
+            center = [ ];
+            right = [
+              "workspace-diagnostics"
+              "selections"
+              "register"
+              "position"
+              "file-encoding"
+              "file-line-ending"
+            ];
+            separator = "│";
+            mode = {
+              normal = "NOR";
+              insert = "INS";
+              select = "SEL";
+            };
+          };
+
+          lsp = {
+            display-messages = true;
+            display-inlay-hints = true;
+          };
+
+          file-picker = {
+            hidden = false;
+          };
+
+          auto-pairs = false;
+
+          indent-guides = {
+            render = true;
+          };
+
+          cursor-shape = {
+            insert = "bar";
+          };
+
+          end-of-line-diagnostics = "hint";
+
+          inline-diagnostics = {
+            cursor-line = "error";
+          };
         };
       };
+
+      languages = {
+        language-server.rust-analyzer = {
+          config = {
+            inlayHints = {
+              typeHints.enable = true;
+              parameterHints.enable = true;
+            };
+          };
+        };
+
+        language = [
+          {
+            name = "nix";
+            auto-format = true;
+          }
+        ];
+      };
+
+      extraPackages = (with pkgs; [
+        lua-language-server
+        marksman
+        nil
+        nixpkgs-fmt
+        shellcheck
+        taplo
+        tinymist
+        yaml-language-server
+      ]) ++ (with pkgs.nodePackages; [
+        bash-language-server
+        vscode-json-languageserver
+      ]);
     };
   };
 
