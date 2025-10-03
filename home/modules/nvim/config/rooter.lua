9b8586d83ff124dada5858ffd05fc16ef5796ce1
Madeleine Mortensen <me@maddie.wtf>
2025-10-03 12:00:55 +0100

    home(nvim): add `.jj/` to nvim rooter patterns

--- home/modules/nvim/config/rooter.lua
+++ home/modules/nvim/config/rooter.lua
@@ -1,3 +1,3 @@
 -- Set file and directory patterns for detection of project root
-vim.g.rooter_patterns = { '.git', '.git/'  }
+vim.g.rooter_patterns = { '.git', '.git/', '.jj/'  }
 vim.g.rooter_cd_cmd = 'lcd'
