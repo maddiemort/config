c6f13a2ba399c0fff6ec4dc91ae04515e958175a
Madeleine Mortensen <me@maddie.wtf>
2026-04-14 08:04:23 +0100

    home(nvim): turn off display of deleted count in signcolumn

--- home/modules/nvim/config/signify.lua
+++ home/modules/nvim/config/signify.lua
@@ -5,6 +5,7 @@ vim.g.signify_sign_change = '▏'
 vim.g.signify_sign_change_delete = '▏'
 vim.g.signify_sign_delete = '▏'
 vim.g.signify_sign_delete_first_line = '▏'
+vim.g.signify_show_count = false
 
 vim.g.signify_skip = {
     vcs = {
