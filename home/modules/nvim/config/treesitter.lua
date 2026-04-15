73813783ebcb01e08436cfd665a3901a135e4670
Madeleine Mortensen <me@maddie.wtf>
2026-04-15 11:44:52 +0100

    home(nvim): remove unnecessary call to nvim-treesitter.configs's setup

--- home/modules/nvim/config/treesitter.lua
+++ home/modules/nvim/config/treesitter.lua
@@ -1,10 +1,3 @@
-require("nvim-treesitter.configs").setup {
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-    },
-}
-
 vim.api.nvim_create_autocmd('FileType', {
     group = vim.api.nvim_create_augroup('enable-treesitter-rust', {}),
     pattern = 'rust',
