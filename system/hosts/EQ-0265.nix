98abcd3196492f2ece7837453a9237bd38698211
Madeleine Mortensen <me@maddie.wtf>
2026-06-23 12:32:37 +0100

    system(EQ-0265): beef up the linux-builder

--- system/hosts/EQ-0265.nix
+++ system/hosts/EQ-0265.nix
@@ -4,7 +4,20 @@
     "zulip"
   ];
 
-  nix.linux-builder.enable = true;
+  nix.linux-builder = {
+    enable = true;
+    ephemeral = true;
+    maxJobs = 4;
+    config = {
+      virtualisation = {
+        darwin-builder = {
+          diskSize = 40 * 1024;
+          memorySize = 8 * 1024;
+        };
+        cores = 6;
+      };
+    };
+  };
 
   services = {
     yknotify-rs = {
